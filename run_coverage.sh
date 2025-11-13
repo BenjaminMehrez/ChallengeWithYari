37b41495ea32a7b50e6dfd4b2954826c1f0518b3
Benjamin Mehrez <benjaminjofre01@gmail.com>
2025-11-13 15:59:54 -0300

    improve run_coverage

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,5 +1,16 @@
+set -e
+
 docker-compose -f docker-compose-test.yml up --build --force-recreate -d
 sleep 5
+
+# Active venv
+if [ -d ".venv" ]; then
+    source .venv/bin/activate
+else
+    python -m venv .venv
+    source .venv/bin/activate
+fi
+
 pytest --cov=app --cov-report=term --cov-report=html
 docker-compose -f docker-compose-test.yml down
 open htmlcov/index.html
